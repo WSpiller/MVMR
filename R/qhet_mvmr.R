aacb4f4d2003d9febc783730a3ec49668522db37
Wes Spiller <ws16063@bristol.ac.uk>
2020-05-29 14:56:22 +0100

    Qhet_boot

--- R/qhet_mvmr.R
+++ R/qhet_mvmr.R
@@ -2,11 +2,16 @@
 #'
 #' Fits a multivariable Mendelian randomization model adjusting for weak instruments. The functions requires a
 #' formatted dataframe using the \code{format_mvmr} function, as well a phenotypic correlation matrix \code{pcor}. This should be obtained from individual level
-#' phenotypic data, or constructed as a correlation matrix where correlations have previously been reported.
+#' phenotypic data, or constructed as a correlation matrix where correlations have previously been reported. Confidence intervals are calculated using a non-parametric bootstrap.
+#' By default, standard errors are not produced but can be calculated by setting \code{se = T}. The number of bootstrap iterations is specified using the \code{iterations argument}.
+#' Note that calculating confidence intervals at present can take a substantial amount of time.
+#' 
 #'
 #'
 #' @param r_input A formatted data frame using the \code{format_mvmr} function.
 #' @param pcor A phenotypic correlation matrix including the correlation between each exposure included in the MVMR analysis.
+#' @param CI Indicates whether 95% confidence intervals should be calculated using a non-parametric bootstrap.
+#' @param iterations Specifies number of bootstrap iterations for calculating 95% confidence intervals.
 #'
 #' @return An dataframe containing effect estimates with respect to each exposure. Currently only point estimates are provided.
 #'@author Wes Spiller; Eleanor Sanderson; Jack Bowden.
@@ -14,32 +19,69 @@
 #'@export
 #'@examples
 #'
-#' qhet_mvmr(r_input,pcor)
+#' qhet_mvmr(r_input,pcor,se=T,iterations=1000)
 #'
 
-qhet_mvmr<-function(r_input,pcor){
-
+qhet_mvmr<-function(r_input,pcor,CI,iterations){
+  
   warning("qhet_mvmr() is currently undergoing development.")
-
-
-
-
+  
+  if(missing(CI)) {
+    CI<-F
+    warning("95% confidence interval not calculated")
+  }
+  
+  if(missing(iterations)) {
+    iterations<-1000
+    warning("Iterations for bootstrap not specified. Default = 1000")
+  }
+  
   exp.number<-length(names(r_input)[-c(1,2,3)])/2
-  stderr<-as.matrix(r_input[,(exp.number + 4):length(r_input)])
-  correlation<-pcor
-  gammahat<-r_input$betaYG
-  segamma<-r_input$sebetaYG
-  pihat<-as.matrix(r_input[,c(4:(3+exp.number))])
-
-  #estimation with the heterogeneity statistic
-
-  PL_MVMR = function(a){
-    tau2   = a[1]
-
-    PL2_MVMR = function(ab){
-      b<-ab
-
-      cov = matrix(nrow = exp.number, ncol = exp.number)
+  
+  Qtemp<-function(r_input,pcor){
+    
+    exp.number<-length(names(r_input)[-c(1,2,3)])/2
+    stderr<-as.matrix(r_input[,(exp.number + 4):length(r_input)])
+    correlation<-pcor
+    gammahat<-r_input$betaYG
+    segamma<-r_input$sebetaYG
+    pihat<-as.matrix(r_input[,c(4:(3+exp.number))])
+    
+    #estimation with the heterogeneity statistic
+    
+    PL_MVMR = function(a){
+      tau2   = a[1]
+      
+      PL2_MVMR = function(ab){
+        b<-ab
+        
+        cov = matrix(nrow = exp.number, ncol = exp.number)
+        w=NULL
+        for(l in 1:nrow(r_input)){
+          for(pp in 1:exp.number){
+            for(p2 in 1:exp.number){
+              cov[pp,p2] <- correlation[pp,p2]*stderr[l,pp]*stderr[l,p2]
+            }
+          }
+          
+          segamma<-r_input$sebetaYG
+          
+          w[l] <- segamma[l]^2+t(b)%*%cov%*%b + tau2
+        }
+        
+        q =  sum((1/w)*((gammahat - pihat%*%b)^2))
+        
+        return(q)
+        
+      }
+      
+      st_PL2  =  rep(0,exp.number)
+      
+      bc    = optim(st_PL2, PL2_MVMR)
+      
+      bcresults <- bc$par
+      
+      cov = matrix(nrow =exp.number, ncol = exp.number)
       w=NULL
       for(l in 1:nrow(r_input)){
         for(pp in 1:exp.number){
@@ -47,91 +89,100 @@ qhet_mvmr<-function(r_input,pcor){
             cov[pp,p2] <- correlation[pp,p2]*stderr[l,pp]*stderr[l,p2]
           }
         }
-
-        segamma<-r_input$sebetaYG
-
-        w[l] <- segamma[l]^2+t(b)%*%cov%*%b + tau2
+        
+        w[l] <- segamma[l]^2+t(bcresults)%*%cov%*%bcresults + tau2
       }
-
+      
+      q = (sum((1/w)*((gammahat - pihat%*%bcresults)^2))-(nrow(r_input)-2))^2
+      
+    }
+    
+    PL2_MVMR = function(ab){
+      b=ab
+      
+      w=NULL
+      cov = matrix(nrow = exp.number, ncol = exp.number)
+      for(l in 1:nrow(r_input)){
+        for(pp in 1:exp.number){
+          for(p2 in 1:exp.number){
+            cov[pp,p2] <- correlation[pp,p2]*stderr[l,pp]*stderr[l,p2]
+          }
+        }
+        
+        w[l] <- segamma[l]^2+t(b)%*%cov%*%b + tau_i
+      }
+      
       q =  sum((1/w)*((gammahat - pihat%*%b)^2))
-
-      return(q)
-
+      
     }
-
-    st_PL2  =  rep(0,exp.number)
-
-    bc    = optim(st_PL2, PL2_MVMR)
-
-    bcresults <- bc$par
-
-    cov = matrix(nrow =exp.number, ncol = exp.number)
-    w=NULL
-    for(l in 1:nrow(r_input)){
-      for(pp in 1:exp.number){
-        for(p2 in 1:exp.number){
-          cov[pp,p2] <- correlation[pp,p2]*stderr[l,pp]*stderr[l,p2]
-        }
-      }
-
-      w[l] <- segamma[l]^2+t(bcresults)%*%cov%*%bcresults + tau2
+    
+    limltauest  = optimize(PL_MVMR,interval=c(-10,10))
+    tau_i = limltauest$objective
+    
+    tau = tau_i
+    
+    liml_het2<- optim(rep(0,exp.number), PL2_MVMR)
+    limlhets <- liml_het2$par
+    Qexact_het <- liml_het2$value
+    
+    Effects<-limlhets
+    Effects<-data.frame(Effects)
+    names(Effects)<-"Effect Estimates"
+    for(i in 1:exp.number){
+      rownames(Effects)[i]<-paste("Exposure",i,sep=" ")
+      
     }
-
-    q = (sum((1/w)*((gammahat - pihat%*%bcresults)^2))-(nrow(r_input)-2))^2
-
+    
+    return(Effects)
+    
+    
   }
-
-  PL2_MVMR = function(ab){
-    b=ab
-
-    w=NULL
-    cov = matrix(nrow = exp.number, ncol = exp.number)
-    for(l in 1:nrow(r_input)){
-      for(pp in 1:exp.number){
-        for(p2 in 1:exp.number){
-          cov[pp,p2] <- correlation[pp,p2]*stderr[l,pp]*stderr[l,p2]
-        }
-      }
-
-      w[l] <- segamma[l]^2+t(b)%*%cov%*%b + tau_i
+  
+  if(CI==F){
+    
+    res<-Qtemp(r_input,pcor)
+    
+  }
+  
+  if(CI==T){
+    
+    bootse<-function(data, indices){
+      
+      bres<-Qtemp(data[indices,],pcor)[,1]
+      
+      return(bres)
+      
     }
-
-    q =  sum((1/w)*((gammahat - pihat%*%b)^2))
-
+    
+    b.results<-boot(data=r_input,statistic=bootse,R=iterations)
+    
+    lcb<-NULL
+    ucb<-NULL
+    ci<-NULL
+    
+    for(i in 1:exp.number){
+      
+      lcb[i]<-round(boot.ci(b.results, type="bca",index=i)$bca[4],digits=3)
+      ucb[i]<-round(boot.ci(b.results, type="bca",index=i)$bca[5],digits=3)
+      
+      ci[i]<-paste(lcb[i],ucb[i],sep="-")
+      
+    }
+    
+    res<-data.frame(b.results$t0,ci)
+    
+    names(res)<-c("Effect Estimates","95% CI")
+    for(i in 1:exp.number){
+      rownames(res)[i]<-paste("Exposure",i,sep=" ")
+      
+    }
+    
   }
-
-  limltauest  = optimize(PL_MVMR,interval=c(-10,10))
-  tau_i = limltauest$objective
-
-  tau = tau_i
-
-  liml_het2<- optim(rep(0,exp.number), PL2_MVMR)
-  limlhets <- liml_het2$par
-  Qexact_het <- liml_het2$value
-
-  Effects<-limlhets
-  Effects<-data.frame(Effects)
-  names(Effects)<-"Effect Estimates"
-  for(i in 1:exp.number){
-    rownames(Effects)[i]<-paste("Exposure",i,sep=" ")
-
-  }
-
-  ##########
-  # Output #
-  ##########
-
-  # Print a few summary elements that are common to both lm and plm model summary objects
-  cat("\n")
-
-  cat("Estimated effects adjusting for weak instruments\n")
-
-  cat("\n")
-
-  print(Effects)
-
-  return(Effects)
-
-
+  
+  return(res)
+ 
+ 
 }
+
+
 
