8404f0803ed395389235439d06ce54740d38634e
Tom Palmer <remlapmot@hotmail.com>
2020-07-28 16:22:55 +0100

    spacing

--- R/snpcov_mvmr.R
+++ R/snpcov_mvmr.R
@@ -21,48 +21,48 @@
 #'
 
 snpcov_mvmr<-function(Gs,Xs){
-  
+
   betas<-matrix(0,ncol=length(Xs[1,]),nrow=length(Gs[1,]))
-  
+
   resmat<-data.frame(rep(0,length(Gs[,1])))
-  
+
   for(j in 1:length(Xs[1,])){
-    
+
     for(i in 1:length(Gs[1,])){
-      
+
       betas[i,j]<-lm(Xs[,j]~-1 + Gs[,i])$coefficients
-      
+
       resids<-data.frame(lm(Xs[,j]~-1 + Gs[,i])$residuals)
-      
+
       resmat<-cbind(resmat,resids)
-      
+
     }
-    
+
   }
-  
+
   resmat<-resmat[,-1]
-  
+
   sigmalist <- vector("list", length(Gs[1,]))
-  
+
   for(i in 1:length(Gs[1,])){
-    
+
     sigma_mattemp<-matrix(((t(Gs[,i]) %*% Gs[,i])^-1)/length(Gs[,i]),ncol=length(Xs[1,])
                           ,nrow=length(Xs[1,]))
-    
+
     for(j in 1:length(Xs[1,])){
-      
+
       for(k in 1:length(Xs[1,])){
-        
+
         sigma_mattemp[k,j]<-sigma_mattemp[k,j] * sum(resmat[,i+((k-1)*length(Gs))] * resmat[,i+((j-1)*length(Gs))])
-        
-        
+
+
       }
     }
-    
+
     sigmalist[[i]] <-sigma_mattemp
-    
+
   }
- 
+
   return(sigmalist)
-   
+
 }
