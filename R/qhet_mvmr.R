386b80cd1cee92a178b0f1b5d005bba05469c786
Tom Palmer <remlapmot@hotmail.com>
2020-07-28 16:20:47 +0100

    import functions

--- R/qhet_mvmr.R
+++ R/qhet_mvmr.R
@@ -17,6 +17,7 @@
 #'@examples
 #' @author Wes Spiller; Eleanor Sanderson; Jack Bowden.
 #' @references Sanderson, E., et al., An examination of multivariable Mendelian randomization in the single-sample and two-sample summary data settings. International Journal of Epidemiology, 2019, 48, 3, 713-727. <https://dx.doi.org/10.1093/ije/dyy262>
+#' @importFrom stats optim optimize
 #'
 #' qhet_mvmr(r_input,pcor,CI=T,iterations=1000)
 #'
