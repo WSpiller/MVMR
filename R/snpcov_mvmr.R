fadd0219d92126b3e871e3deac626557b8a18e1e
Tom Palmer <remlapmot@hotmail.com>
2020-07-28 16:22:32 +0100

    format reference

--- R/snpcov_mvmr.R
+++ R/snpcov_mvmr.R
@@ -8,14 +8,13 @@
 #' @param Xs A matrix or dataframe containing exposure measures. Columns should indicate exposure number, with rows representing an observed measure for the given exposure.
 #'
 #' @return A list of covariance matrices with respect to each genetic variant, retaining the ordering in \code{Gs}
-#' 
-#'@author Wes Spiller; Eleanor Sanderson; Jack Bowden.
-#'@references Sanderson, E., et al., An examination of multivariable Mendelian randomization in the single-sample and two-sample summary data settings. International Journal of Epidemiology, 2018. [Internet]. 2018;dyy262. Available from: https://dx.doi.org/10.1093/ije/dyy262
 #'@export
 #'@examples
 #'
 #' snpcov_mvmr(data[,1:10],data[,11:13])
 #' 
+#' @author Wes Spiller; Eleanor Sanderson; Jack Bowden.
+#' @references Sanderson, E., et al., An examination of multivariable Mendelian randomization in the single-sample and two-sample summary data settings. International Journal of Epidemiology, 2019, 48, 3, 713-727. <https://dx.doi.org/10.1093/ije/dyy262>
 #' @importFrom stats lm
 
 snpcov_mvmr<-function(Gs,Xs){
