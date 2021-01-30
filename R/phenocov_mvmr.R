b6cc69d4368e37b68d95cd90c97368de10f00b4b
Tom Palmer <remlapmot@hotmail.com>
2021-01-30 10:46:13 +0000

    \doi

--- R/phenocov_mvmr.R
+++ R/phenocov_mvmr.R
@@ -10,10 +10,10 @@
 #' @return A list of covariance matrices with respect to each genetic variant, retaining the ordering in \code{seBXGs}
 #'
 #'@author Wes Spiller; Eleanor Sanderson; Jack Bowden.
-#'@references Sanderson, E., et al., An examination of multivariable Mendelian randomization in the single-sample and two-sample summary data settings. International Journal of Epidemiology, 2019, 48, 3, 713-727. <https://dx.doi.org/10.1093/ije/dyy262>
 #'@export
 #'@examples
 #'
+#' @references Sanderson, E., et al., An examination of multivariable Mendelian randomization in the single-sample and two-sample summary data settings. International Journal of Epidemiology, 2019, 48, 3, 713-727. \doi{10.1093/ije/dyy262}
 #' \dontrun{
 #' phenocov_mvmr(Pcov, summarydata[,c(3,4)])
 #' }
