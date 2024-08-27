710cb915476700abd1eec43160925e4cfbc42686
Tom Palmer <remlapmot@hotmail.com>
2024-08-27 14:39:35 +0100

    Use seq_along()

--- R/strhet_mvmr.R
+++ R/strhet_mvmr.R
@@ -114,7 +114,7 @@ strhet_mvmr<-function(r_input,gencov){
 
             temp<-temp[-i]
 
-            for(l in 1:length(r_input[,1])){
+            for(l in seq_along(r_input[,1])){
 
               sigma2xj_dat[l,i]<- sigma2xj_dat[l,i] - delta_mat[j,i]*2*gencov[[l]][i,temp[k]]
 
